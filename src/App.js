a194900493f16d3c18252cf87595768fae33e4b5
Joshua Immordino <Joshimmor@gmail.com>
2022-12-31 19:45:30 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
 import logo from './logo.svg';
 import './App.css';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { GoogleMapsOverlay } from "@deck.gl/google-maps";
+import { GeoJsonLayer} from '@deck.gl/layers';
+import { MapStyles } from './MapStyles';
+
+
 
 function App() {
+  const mapStyles = {
+  height: "100vh",
+  width: "100%" ,
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GoogleMap
+      onLoad={map => {
+        deckOverlay.setMap(map)
+      }}
+      
+      options={{
+        styles: MapStyles,
+        disableDefaultUI: true,
+      }}
+      mapContainerStyle={mapStyles}
+      
+      zoom={13}
+      center={{lat: 40.712776, lng: -74.005974}}
+      />
+
     </div>
   );
 }
 
+const NYC_QUEENS_22 = "./nycqueens2022.json"
+const NYC_BROOKLYN_22 = "./nycbrooklyn22.json"
+const deckOverlay = new GoogleMapsOverlay({
+	layers: [
+    new GeoJsonLayer({
+      id: "QueensHousing",
+      data: NYC_QUEENS_22,
+      filled: true,
+      pointRadiusMinPixels: 5,
+      opacity: 1,
+      pointRadiusScale: 2,
+      Radius: f => 3,
+      getFillColor: [200, 0, 80, 180],
+      pickable: true,
+      }),
+
+      new GeoJsonLayer({
+      id: "BrooklynHousing",
+      data: NYC_BROOKLYN_22,
+      filled: true,
+      pointRadiusMinPixels: 5,
+      opacity: 1,
+      pointRadiusScale: 2,
+      Radius: f => 3,
+      getFillColor: [200, 0, 80, 180],
+      pickable: true,
+      autoHighlight: true,
+      })
+	  ],
+    getTooltip: ({object}) => {
+      return object && {
+      html: `<div>
+      <h3>Project Name: ${object.properties['Project Name']}</h3> 
+      <h3>Project Start Date: ${object.properties['Project Start Date']}</h3> 
+      <h3>Borough: ${object.properties['Borough']}</h3> 
+      <h3>Extremely Low Income Units: ${object.properties['Extremely Low Income Units']}</h3> 
+      <h3>Very Low Income Units: ${object.properties['Very Low Income Units']}</h3> 
+      <h3>Low Income Units: ${object.properties['Low Income Units']}</h3> 
+      <h3>Moderate Income Units: ${object.properties['Moderate Income Units']}</h3> 
+      <h3>Middle Income Units: ${object.properties['Middle Income Units']}</h3> 
+      <h3>Studio Units: ${object.properties['Studio Units']}</h3> 
+      <h3>1-BR Units: ${object.properties['1-BR Units']}</h3> 
+      <h3>2-BR Units: ${object.properties['2-BR Units']}</h3> 
+      <h3>3-BR Units: ${object.properties['3-BR Units']}</h3> 
+      <h3>4-BR Units: ${object.properties['4-BR Units']}</h3> 
+      </div>`,
+      style: {
+        backgroundColor: '#0075A',
+        color: '#FEFAE0',
+        width: '10vw',
+        textAlign:"start",
+        fontSize: '0.8em'
+      } 
+     }
+    }
+  });
+  
 export default App;
---
+++ src/MapStyles.js
@@ -0,0 +1,76 @@
+export const MapStyles = [
+    { elementType: "geometry", stylers: [{ color: "#FEFAE0" }] },
+    { elementType: "labels.text.stroke", stylers: [{ visibility: "off" }] },
+    { elementType: "labels.text.fill", stylers: [{ visibility: "off" }]},
+    {
+      elementType: "labels.icon",
+      stylers: [{ visibility: "off" }],
+    },
+    {
+      featureType: "administrative.locality",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#d59563" }],
+    },
+    {
+      featureType: "poi",
+      elementType: "labels.text.fill",
+      stylers: [{ visibility: "off" }],
+    },
+    {
+      featureType: "poi.park",
+      elementType: "geometry",
+      stylers: [{ color: "#ccd5ae" }],
+    },
+    {
+      featureType: "poi.park",
+      elementType: "labels.text.fill",
+      stylers: [{ visibility: "off" }],
+    },
+  
+    {
+      featureType: "road",
+      elementType: "geometry",
+      stylers: [{ color: "#e9edc9" }],
+    },
+    {
+      featureType: "road",
+      elementType: "geometry.stroke",
+      stylers: [{ color: "#e9edc9" }],
+    },
+    {
+      featureType: "road",
+      elementType: "labels.text.fill",
+      stylers: [{ visibility: "off" }],
+    },
+    {
+      featureType: "road.highway",
+      elementType: "geometry",
+      stylers: [{ color: "#e9edc9" }],
+    },
+    {
+      featureType: "road.highway",
+      elementType: "geometry.stroke",
+      stylers: [{ visibility: "off" }],
+    },
+    {
+      featureType: "road.highway",
+      elementType: "labels.text.fill",
+      stylers: [{ visibility: "off" }],
+    },
+   
+    {
+      featureType: "water",
+      elementType: "geometry",
+      stylers: [{ color: "#7ca8b6" }],
+    },
+    {
+      featureType: "water",
+      elementType: "labels.text.fill",
+      stylers: [{ visibility: "off" }],
+    },
+    {
+      featureType: "water",
+      elementType: "labels.text.stroke",
+      stylers: [{ visibility: "off" }],
+    },
+];
